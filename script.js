ca5ccf8d6f6150c1b4129d62f2c7b246960a0788
thakureechand213-cpu <thakureechand213@gmail.com>
2025-09-24 15:40:54 +0530

    Updated tea name and added choco shake

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// ----------------------
+// Wi-Fi Sidebar
+// ----------------------
 const wifiBtn = document.getElementById("wifi-btn");
 const wifiSidebar = document.getElementById("wifi-sidebar");
 const closeBtn = document.getElementById("close-sidebar");
@@ -17,16 +20,37 @@ const copyBtn = document.getElementById("copy-btn");
 const wifiPassword = document.getElementById("wifi-password");
 const copyMsg = document.getElementById("copy-msg");
 
-if (copyBtn) { // make sure element exists
+if (copyBtn) {
   copyBtn.addEventListener("click", () => {
     navigator.clipboard.writeText(wifiPassword.textContent).then(() => {
-      // Show success message
       copyMsg.style.display = "inline";
       setTimeout(() => {
         copyMsg.style.display = "none";
-      }, 2000); // hide after 2 seconds
+      }, 2000);
     }).catch(err => {
       console.log("Failed to copy!", err);
     });
   });
 }
+
+// ----------------------
+// Review Modal
+// ----------------------
+const reviewBtn = document.getElementById("review-btn");
+const reviewModal = document.getElementById("review-modal");
+const closeModal = document.getElementById("close-modal");
+
+reviewBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  reviewModal.style.display = "flex";
+});
+
+closeModal.addEventListener("click", () => {
+  reviewModal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === reviewModal) {
+    reviewModal.style.display = "none";
+  }
+});
